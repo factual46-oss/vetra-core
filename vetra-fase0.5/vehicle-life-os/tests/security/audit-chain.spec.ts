b1741efb8783071b3ac13e5b3e60392db779ae8b
factual46-oss <factual46@gmail.com>
2026-08-18 12:00:11 -0300

    Update audit-chain.spec.ts

--- vetra-fase0.5/vehicle-life-os/tests/security/audit-chain.spec.ts
+++ vetra-fase0.5/vehicle-life-os/tests/security/audit-chain.spec.ts
@@ -12,7 +12,7 @@ import { APP_URL, HAS_DB, MIGRATOR_URL, connect, expectRejection } from '../help
  * E os privilegios provam uma terceira: a aplicacao escreve no log, mas nao o le
  * nem o altera.
  */
-describe.skipIf(!HAS_DB)('cadeia de hash do log auditavel', () => {
+describe (!HAS_DB)('cadeia de hash do log auditavel', () => {
   let db: pg.Client;
   let firstId: number;
 
